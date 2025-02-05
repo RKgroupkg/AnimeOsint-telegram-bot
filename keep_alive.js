332f5bf966bd21db4c4a08ad5489600bde152894
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-02-05 12:49:46 +0530

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,9 +1,13 @@
-import http from 'http';
+const http = require('http');
 
-// Create a basic HTTP server
 http.createServer(function (req, res) {
-  res.write("I'm alive"); // Respond to incoming requests
-  res.end();
+  if (req.url === '/ping') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write("I'm alive");
+    res.end();
+  } else {
+    console.log("something else ping");
+  }
 }).listen(process.env.PORT || 8080, () => {
   console.log("Keep-alive server is running!");
 });
